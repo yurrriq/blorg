980508bb5dc2cfef6a9aa067f52aec49812846e0
Eric Bailey <eric@ericb.me>
2015-12-12 23:30:15 -0600

    Update config

--- blorg.el
+++ blorg.el
@@ -15,32 +15,31 @@
   (setq org-publish-project-alist '(())))
 
 ;; Reference: http://orgmode.org/worg/org-tutorials/org-jekyll.html#text-4
-(unless (assoc "blorg" org-publish-project-alist)
-  (add-to-list
-   'org-publish-project-alist
-   '("org-blorg"
-     :base-directory "~/src/yurrriq/blorg/org/"
-     :base-extension "org"
-     :publishing-directory "~/src/yurrriq/blorg/jekyll/"
-     :publishing-function org-html-publish-to-html
-     :exclude "\\(README\\|setup\\).org"
-     :fontify-natively t
-     :recursive t
-     :section-numbers nil
-     :html-doctype "html5"
-     :html-extension "html"
-     :htmlized-source t
-     :body-only t))
-  (add-to-list
-   'org-publish-project-alist
-   '("org-static-blorg"
-     :base-directory "~/src/yurrriq/blorg/org/"
-     :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|svg\\|mp3"
-     :publishing-directory "~/src/yurrriq/blorg/jekyll/"
-     :recursive t
-     :publishing-function org-publish-attachment))
-  (add-to-list
-   'org-publish-project-alist
-   '("blorg" :components ("org-blorg" "org-static-blorg"))))
+(add-to-list
+ 'org-publish-project-alist
+ '("org-blorg"
+   :base-directory "~/src/yurrriq/blorg/org/"
+   :base-extension "org"
+   :publishing-directory "~/src/yurrriq/blorg/jekyll/"
+   :publishing-function org-html-publish-to-html
+   :exclude "\\(README\\|setup\\).org"
+   :fontify-natively t
+   :recursive t
+   :section-numbers nil
+   :html-doctype "html5"
+   :html-extension "html"
+   :htmlized-source t
+   :body-only t))
+(add-to-list
+ 'org-publish-project-alist
+ '("org-static-blorg"
+   :base-directory "~/src/yurrriq/blorg/org/"
+   :base-extension "css\\|js\\|png\\|jpg\\|gif\\|ly\\|pdf\\|scm\\|svg\\|mp3"
+   :publishing-directory "~/src/yurrriq/blorg/jekyll/"
+   :recursive t
+   :publishing-function org-publish-attachment))
+(add-to-list
+ 'org-publish-project-alist
+ '("blorg" :components ("org-blorg" "org-static-blorg")))
 
 ;;; blorg.el ends here
