b78698742363fc5e6c6d806167674f0a7758a16c
Eric Bailey <eric@ericb.me>
2018-05-07 20:11:46 -0500

    Update Org syntax and add post

--- hakyll/default.nix
+++ hakyll/default.nix
@@ -1,4 +1,4 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default", doBenchmark ? false }:
 
 let
 
@@ -8,22 +8,23 @@ let
       }:
       mkDerivation {
         pname = "blorg";
-        version = "0.1.0.0";
+        version = "0.2.0.0";
         src = ./.;
         isLibrary = false;
         isExecutable = true;
         executableHaskellDepends = [ base filepath hakyll split text ];
-        buildDepends = if stdenv.isDarwin
-                          then [ pkgs.darwin.apple_sdk.frameworks.Cocoa ]
-                          else [];
-        license = stdenv.lib.licenses.bsd3;
+        license = stdenv.lib.licenses.unfree;
+        hydraPlatforms = stdenv.lib.platforms.none;
+        maintainers = with stdenv.lib.maintainers; [ yurrriq ];
       };
 
   haskellPackages = if compiler == "default"
                        then pkgs.haskellPackages
                        else pkgs.haskell.packages.${compiler};
 
-  drv = haskellPackages.callPackage f {};
+  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
+
+  drv = variant (haskellPackages.callPackage f {});
 
 in
 
