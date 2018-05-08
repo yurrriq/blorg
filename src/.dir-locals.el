60f343e0347262f124bdcdfcd889fcad3159e78a
Eric Bailey <eric@ericb.me>
2018-05-08 00:37:52 -0500

    htmlize latest post

---
+++ src/.dir-locals.el
@@ -0,0 +1,7 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((org-mode
+  (org-src-fontify-natively . t)
+  (org-src-preserve-indentation . t)))
+
